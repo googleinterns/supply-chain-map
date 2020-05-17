efc9b9bd73bb6340dfab700d9c45df587e8935de
Nirup Anandaraman <nirup@google.com>
2020-05-17 14:37:19 -0400

    App Component clean up

--- web/scm/src/app/app-component/app.component.spec.ts
+++ web/scm/src/app/app-component/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('scm');
   });
 
-  it('should render title', () => {
+  it('should render para', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('scm app is running!');
+    expect(compiled.querySelector('p').textContent).toContain('Hello');
   });
 });
