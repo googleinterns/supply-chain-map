19ad9417ba98048e202a7e230f5b15090a08ef64
Nirup Anandaraman <nirup@google.com>
2020-05-18 15:54:45 -0400

    Reqrite to follow style guide

--- web/scm/src/app/app.module.ts
+++ web/scm/src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { AgmCoreModule } from '@agm/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { AgmCoreModule } from '@agm/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-component/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -18,12 +16,12 @@ import { environment } from '../environments/environment';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
     AgmCoreModule.forRoot({
       apiKey: environment.googleMapApi
     }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
