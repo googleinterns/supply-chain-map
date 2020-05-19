493d3bfa767d7417156c06c1e1cd72f775845e37
Nirup Anandaraman <nirup@google.com>
2020-05-19 12:19:49 -0400

    Move map to its own component

--- web/scm/src/app/app-component/app.component.spec.ts
+++ web/scm/src/app/app-component/app.component.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AgmCoreModule } from '@agm/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
+      imports: [
+        AgmCoreModule.forRoot({
+          apiKey: environment.googleMapApi
+        }),
+        BrowserAnimationsModule,
+        BrowserModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
 
@@ -26,10 +44,17 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('scm');
   });
 
-  it('should display hello message', () => {
+  it('toolbar should display \'Supply Chain Map\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('p').textContent).toContain('Hello');
+    expect(compiled.querySelector('h1.product-name').textContent).toContain('Supply Chain Map');
+  });
+
+  it('sidenav should display \'Side nav bar\'', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('mat-sidenav p').textContent).toContain('Side nav bar');
   });
 });
