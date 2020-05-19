268c98bdc681ff2641b93b6e5aa7f931a8e63cbf
dpronin <dpronin@google.com>
2020-05-18 17:33:45 -0700

    Update karma.conf.js

--- web/scm/karma.conf.js
+++ web/scm/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['HeadlessChrome'],
+    customLaunchers: {
+      HeadlessChrome: {
+        base: 'Chrome',
+        flags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
