d83729c2c92b4439faf657f46c55cacb846ac79d
dpronin <dpronin@google.com>
2020-05-19 18:24:16 -0700

    Revert "Added logo and changed tests layout"
    This reverts commit e059cc7588349435a63e45042c387c6ad60f2dfe.

--- web/scm/src/app/app-component/app.component.spec.ts
+++ web/scm/src/app/app-component/app.component.spec.ts
@@ -3,9 +3,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  var fixture;
-  var component;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -15,21 +12,24 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
   }));
 
   it('should create the app', () => {
-    expect(component).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'scm'`, () => {
-    expect(component.title).toEqual('scm');
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('scm');
   });
 
   it('should display hello message', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('p').textContent).toContain('Side nav bar');
+    expect(compiled.querySelector('p').textContent).toContain('Hello');
   });
 });
