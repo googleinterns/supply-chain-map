9144dc7d10bca7c85cec660a65f8fa103eed50ce
Nirup Anandaraman <nirup@google.com>
2020-05-20 11:48:49 -0400

    Revert "Revert "Fixed tests to pass CI and added missing files""
    This reverts commit a66acdde0333da73b772285585618b34d376da94.

--- web/scm/src/app/app-component/app.component.spec.ts
+++ web/scm/src/app/app-component/app.component.spec.ts
@@ -1,35 +1,68 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AgmCoreModule } from '@agm/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { MapComponent } from '../map/map.component';
+import { SidePanelComponent } from '../side-panel/side-panel.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MapComponent,
+        SidePanelComponent
       ],
+      imports: [
+        AgmCoreModule.forRoot({
+          apiKey: environment.googleMapApi
+        }),
+        BrowserAnimationsModule,
+        BrowserModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        RouterTestingModule
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'scm'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('scm');
+    expect(component.title).toEqual('scm');
   });
 
-  it('should display hello message', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('toolbar should display \'Supply Chain Map\'', () => {
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('p').textContent).toContain('Hello');
+    expect(compiled.querySelector('h1.product-name').textContent).toContain('Supply Chain Map');
+  });
+
+  it('should display side panel', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('scm-side-panel')).toBeTruthy();
+  });
+
+  it('should display map', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('agm-map')).toBeTruthy();
   });
 });
