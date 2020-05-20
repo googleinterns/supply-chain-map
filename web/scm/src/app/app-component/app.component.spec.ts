a66acdde0333da73b772285585618b34d376da94
Nirup Anandaraman <nirup@google.com>
2020-05-20 11:44:12 -0400

    Revert "Fixed tests to pass CI and added missing files"
    This reverts commit 95f02cb115905d19b689a4b232cfc75a80829cc6.

--- web/scm/src/app/app-component/app.component.spec.ts
+++ web/scm/src/app/app-component/app.component.spec.ts
@@ -1,68 +1,35 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AgmCoreModule } from '@agm/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
-import { MapComponent } from '../map/map.component';
-import { SidePanelComponent } from '../side-panel/side-panel.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent,
-        MapComponent,
-        SidePanelComponent
-      ],
       imports: [
-        AgmCoreModule.forRoot({
-          apiKey: environment.googleMapApi
-        }),
-        BrowserAnimationsModule,
-        BrowserModule,
-        MatButtonModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatToolbarModule,
         RouterTestingModule
-      ]
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    expect(component).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'scm'`, () => {
-    expect(component.title).toEqual('scm');
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('scm');
   });
 
-  it('toolbar should display \'Supply Chain Map\'', () => {
+  it('should display hello message', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1.product-name').textContent).toContain('Supply Chain Map');
-  });
-
-  it('should display side panel', () => {
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('scm-side-panel')).toBeTruthy();
-  });
-
-  it('should display map', () => {
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('agm-map')).toBeTruthy();
+    expect(compiled.querySelector('p').textContent).toContain('Hello');
   });
 });
--- web/scm/src/app/app.module.ts
+++ web/scm/src/app/app.module.ts
@@ -10,14 +10,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app-component/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
-import { MapComponent } from './map/map.component';
-import { SidePanelComponent } from './side-panel/side-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MapComponent,
-    SidePanelComponent
+    AppComponent
   ],
   imports: [
     AgmCoreModule.forRoot({
