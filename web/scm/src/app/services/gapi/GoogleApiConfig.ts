82c3f2c53badce27508b1d3a63c36cf1a44f2749
Nirup Anandaraman <nirup@google.com>
2020-05-28 13:26:12 -0400

    Added Gapi services

---
+++ web/scm/src/app/services/gapi/GoogleApiConfig.ts
@@ -0,0 +1,17 @@
+import ClientConfig = gapi.auth2.ClientConfig;
+
+export interface NgGapiClientConfig extends ClientConfig {
+    discoveryDocs: string[];
+}
+
+export class GoogleApiConfig {
+    protected clientConfig: NgGapiClientConfig;
+
+    constructor(clientConfig: NgGapiClientConfig) {
+        this.clientConfig = clientConfig;
+    }
+
+    public getClientConfig(): NgGapiClientConfig {
+        return this.clientConfig;
+    }
+}
