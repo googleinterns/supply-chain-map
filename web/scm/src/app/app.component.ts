a63887727946c0f1e88fd1270e11facce7ff38d0
Nirup Anandaraman <nirup@google.com>
2020-05-28 11:18:15 -0400

    Cosmetic fixes

--- web/scm/src/app/app.component.ts
+++ web/scm/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'scm-root',
-  templateUrl: './app.component.html',
-  styleUrls: []
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 }
