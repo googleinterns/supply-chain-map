a2241fd7031aac9081c67310fc143b2b87516a76
Nirup Anandaraman <nirup@google.com>
2020-07-18 17:41:47 -0400

    Fixed tests

--- web/scm/src/app/home/dashboard/tabs/graph-tab/graph-tab.component.spec.ts
+++ web/scm/src/app/home/dashboard/tabs/graph-tab/graph-tab.component.spec.ts
@@ -1,16 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphTabComponent } from './graph-tab.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { MemoizedSelector } from '@ngrx/store';
+import { FormQueryResult } from 'src/app/home/home.models';
+import { HomeState } from 'src/app/home/store/state';
+import { selectHomeFormQueryResult } from 'src/app/home/store/selectors';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('GraphTabComponent', () => {
   let component: GraphTabComponent;
   let fixture: ComponentFixture<GraphTabComponent>;
+  let mockStore: MockStore;
+  let mockFormQuerySelector: MemoizedSelector<HomeState, FormQueryResult>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GraphTabComponent ]
+      imports: [ MatDialogModule ],
+      declarations: [ GraphTabComponent ],
+      providers: [
+        provideMockStore()
+      ]
     })
     .compileComponents();
+
+    mockStore = TestBed.inject(MockStore);
+    mockFormQuerySelector = mockStore.overrideSelector(
+      selectHomeFormQueryResult,
+      {
+        upstream: [],
+        cm: [],
+        downstream: []
+      }
+    );
   }));
 
   beforeEach(() => {
--- web/scm/src/app/home/map/layers/route-layer/route-layer.component.spec.ts
+++ web/scm/src/app/home/map/layers/route-layer/route-layer.component.spec.ts
@@ -1,10 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { RouteLayerComponent } from './route-layer.component';
+import { MemoizedSelector } from '@ngrx/store';
+import { HomeState } from 'src/app/home/store/state';
+import { FormQueryResult } from 'src/app/home/home.models';
+import { selectHomeFormQueryResult } from 'src/app/home/store/selectors';
 
 describe('RouteLayerComponent', () => {
   let component: RouteLayerComponent;
   let fixture: ComponentFixture<RouteLayerComponent>;
+  let mockStore: MockStore;
+  let mockFormQuerySelector: MemoizedSelector<HomeState, FormQueryResult>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +21,16 @@ describe('RouteLayerComponent', () => {
       ]
     })
     .compileComponents();
+
+    mockStore = TestBed.inject(MockStore);
+    mockFormQuerySelector = mockStore.overrideSelector(
+      selectHomeFormQueryResult,
+      {
+        upstream: [],
+        cm: [],
+        downstream: []
+      }
+    );
   }));
 
   beforeEach(() => {
